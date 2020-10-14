33237c5fa5a4ec991352433ef44fa35c03162dfa
PulakRaj <prpulak@gmail.com>
2020-10-14 19:45:07 -0400

    time

--- project1/app.py
+++ project1/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import psycopg2
+import time
 
 # MySQL setup
 app = Flask(__name__)
@@ -19,9 +20,11 @@ redshift = psycopg2.connect(dbname = 'instacart',
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    elapsed_time = 0
     try:
         fetchdata = ''
         if request.method == 'POST':
+            start_time = time.time()
             query_details = request.form
             query = query_details['my_query']
             if query_details['options'] == 'my_mysql':
@@ -30,16 +33,19 @@ def index():
                 mysql.connection.commit()
                 fetchdata = cur.fetchall()
                 cur.close
+                elapsed_time = time.time() - start_time
             elif query_details['options'] == 'my_redshift':
                 cur = redshift.cursor()
                 cur.execute(query)
                 fetchdata = cur.fetchall()
                 cur.close()
+                elapsed_time = time.time() - start_time
     except mysql.connect.DatabaseError:
         fetchdata = "There is an error in your SQL syntax."
     except:
         fetchdata = "There is an error in your program."
-    return render_template('project1.html', data = fetchdata)
+    return render_template('project1.html', data = fetchdata, time = elapsed_time)
+
             
 if __name__ == "__main__":
     app.run(debug=True)
